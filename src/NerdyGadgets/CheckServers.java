e14ad80bc12067e009f7770ee00d02641c638a73
Jelte Broer <jelte.broer@gmail.com>
2019-05-21 12:15:09 +0200

    Testscript netwerk

--- src/NerdyGadgets/CheckServers.java
+++ src/NerdyGadgets/CheckServers.java
@@ -1,9 +1,7 @@
 package NerdyGadgets;
 
 import java.io.*;
-import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -13,25 +11,37 @@ import java.util.List;
 import java.util.Properties;
 
 public class CheckServers {
-    public ArrayList<String> ipAddresses = new ArrayList<>();
+    private ArrayList<String> ipAddresses = new ArrayList<>();
+    private String wanIpAddress;
 
     public CheckServers(){
-        getConfig();
-        for (String ipAddress : ipAddresses){
-            try{
-                InetAddress a = InetAddress.getByName(ipAddress);
-                if(a.isReachable(2000)){
-                    System.out.println(ipAddress + " works!");
-                }else {
-                    System.out.println(ipAddress + " is not reachable!");
+        if(getConfig()){
+            System.out.println("Checking local servers:");
+            for (String ipAddress : ipAddresses){
+                try{
+                    InetAddress a = InetAddress.getByName(ipAddress);
+                    if(a.isReachable(2000)){
+                        System.out.println(ipAddress + " works!");
+                    }else {
+                        System.out.println(ipAddress + " is not reachable!");
+                    }
+                }catch (Exception e){
+                    System.out.println(e.getMessage());
                 }
-            }catch (UnknownHostException e){
-                System.out.println(e.getMessage());
+
+            }
+
+            try{
+                if(InetAddress.getByName("your server url").isReachable(2000)){
+                    System.out.println("Your network is reachable from the wan via " + wanIpAddress);
+                }else {
+                    System.out.println("Your network is not reachable via wan " + wanIpAddress);
+                }
             }catch (Exception e){
                 System.out.println(e.getMessage());
             }
-
         }
+
     }
 
 
@@ -50,7 +60,7 @@ public class CheckServers {
         try{
             is = new FileInputStream("ipAddresses.config");
         }catch (FileNotFoundException e){
-            List<String> lines = Arrays.asList("# Write all the ip addresses in this file behind 'ipAddresses=' separated by commas.", "ipAddresses=");
+            List<String> lines = Arrays.asList("# Write all the ip addresses in this file behind 'ipAddresses=' separated by commas.", "ipAddresses=", "# The wan address is the address provided by your Internet Service Provider", "wanAddress=");
             try{
                 conf.setExecutable(true);
                 conf.setReadable(true);
@@ -80,9 +90,11 @@ public class CheckServers {
                 ipAddresses.add(s);
             }
 
+            wanIpAddress = prop.getProperty("wanAddress");
 
-        }catch(NumberFormatException e){
-            System.out.println("Make sure your put an interger in the serverStatus.conf serverID or databaseLookback property");
+
+        }catch(Exception e){
+            System.out.println("Make sure your ipAddresses.config has the right format");
             return false;
 
         }
